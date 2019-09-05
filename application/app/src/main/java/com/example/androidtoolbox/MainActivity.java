cb0ab72d002d05c74b742c06d3bfda7da09f9714
zsl <742706540@qq.com>
2019-09-05 15:44:32 +0800

    create linear layout demo

--- application/app/src/main/java/com/example/androidtoolbox/MainActivity.java
+++ application/app/src/main/java/com/example/androidtoolbox/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.androidtoolbox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.androidtoolbox.layout.LinearLayoutActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+
+    private List<Pair> titles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titles.add(new Pair("LinearLayout", new Intent(this, LinearLayoutActivity.class)));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(titles));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    private static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.tv_title);
+        }
+    }
+
+    private static class Pair {
+        public String title;
+        public Intent intent;
+
+        public Pair(String title, Intent intent) {
+            this.title = title;
+            this.intent = intent;
+        }
+
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        private List<Pair> dataList;
+
+        public MyAdapter(List<Pair> data) {
+            dataList = data;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_item1, viewGroup, false);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, final int i) {
+            myViewHolder.textView.setText(dataList.get(i).title);
+            myViewHolder.textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(dataList.get(i).intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
     }
 }
