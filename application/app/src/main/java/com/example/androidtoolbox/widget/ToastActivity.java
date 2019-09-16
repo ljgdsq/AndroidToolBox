b8087935dc1536818eeb4208597fdff70ecdef0b
zsl <742706540@qq.com>
2019-09-16 17:52:38 +0800

    添加自定义Toast

--- application/app/src/main/java/com/example/androidtoolbox/widget/ToastActivity.java
+++ application/app/src/main/java/com/example/androidtoolbox/widget/ToastActivity.java
@@ -2,14 +2,19 @@ package com.example.androidtoolbox.widget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.androidtoolbox.R;
+import com.example.androidtoolbox.utils.ImageUtils;
 
 public class ToastActivity extends AppCompatActivity {
 
@@ -19,11 +24,10 @@ public class ToastActivity extends AppCompatActivity {
         setContentView(R.layout.activity_toast);
 
 
-
     }
 
-    private void defaultToast(){
-        Toast.makeText(this,"Toast",Toast.LENGTH_LONG).show();
+    private void defaultToast() {
+        Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show();
     }
 
     public void showDefaultToast(View view) {
@@ -31,11 +35,11 @@ public class ToastActivity extends AppCompatActivity {
     }
 
 
-    private void toast01()
-    {
-        Toast toast=Toast.makeText(this,"Toast",Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.START,100,100);
-        TextView textView=toast.getView().findViewById(android.R.id.message);
+    private void toast01() {
+        Toast toast = Toast.makeText(this, "Toast", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.START, 100, 100);
+        TextView textView = toast.getView().findViewById(android.R.id.message);
+
         textView.setTextColor(Color.RED);
         toast.show();
     }
@@ -43,5 +47,39 @@ public class ToastActivity extends AppCompatActivity {
 
     public void showToast01(View view) {
         toast01();
+    }
+
+    public void showToast02(View view) {
+        toast02();
+    }
+
+
+    private void toast02() {
+        Toast toast = Toast.makeText(this, "Toast", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 0);
+        LinearLayout linearLayout = (LinearLayout) toast.getView();
+        linearLayout.setBackgroundColor(Color.BLUE);
+        ImageView imageView = new ImageView(this);
+
+        Bitmap scaledBitmap = ImageUtils.getScaledBitmap(getResources(), R.drawable.icon_edit, 64, 64);
+        imageView.setImageBitmap(scaledBitmap);
+        linearLayout.addView(imageView, 0);
+        toast.show();
+    }
+
+
+
+    private void toast03()
+    {
+        Toast toast = new Toast(this);
+
+        LayoutInflater layoutInflater=getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.toast, null);
+        toast.setView(view);
+        toast.show();
+    }
+
+    public void showToast03(View view) {
+        toast03();
     }
 }
